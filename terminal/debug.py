3c1b5c2ae2c656edbef92eff76f14f0f8e550ec9
jack <jack@example.com>
2025-11-13 09:42:27 +0800

    重构：terminel部分目录重构

--- terminal/debug.py
+++ terminal/debug.py
@@ -1,21 +1,21 @@
 from pathlib import Path
 
 def test_create_plugin():
-    import py_plugin_tools.ops_create_plugin as test
+    import pyside_plugin_maker.ops_create_plugin as test
     plugin_dir = Path(r"C:\Users\user1\Desktop\Code\test")
     plugin_name = "test_demo"
     feat_name = "feat_demo"
     test.create_plugin(plugin_dir, plugin_name, feat_name)
 
 def test_add_feat():
-    import py_plugin_tools.ops_add_plugin_feat as test
+    import pyside_plugin_maker.ops_add_plugin_feat as test
     plugin_dir = Path(r"C:\Users\user1\Desktop\Code\test")
     plugin_name = "test"
     feat_name = "demo5"
     test.add_plugin_feat(plugin_dir, plugin_name, feat_name)
 
 def test_del_feat():
-    import py_plugin_tools.ops_del_plugin_feat as test
+    import pyside_plugin_maker.ops_del_plugin_feat as test
     plugin_dir = Path(r"C:\Users\user1\Desktop\Code\test")
     plugin_name = "test"
     feat_name = "demo1"
---
+++ terminal/project_maker/readme.md
@@ -0,0 +1,2 @@
+项目创建脚手架
+pyside_plugin_maker: 用于创建 PySide6 插件项目的脚手架工具。
